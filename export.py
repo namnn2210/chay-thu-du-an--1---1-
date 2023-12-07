55a14b67cdb236797f2383fa13489addfb43b28b
namng <nam.nn@bidgear.com>
2023-12-08 00:44:11 +0700

    edit

--- export.py
+++ export.py
@@ -1,20 +1,240 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_excel('a.xlsx')
+product_data = pd.read_excel('CSDL_MON.xlsx')
 
-total_sold_by_product_df = df.drop(columns=['Thành tiền', 'Thời gian', 'Khách hàng', 'SĐT']).sort_values('Số lượng', ascending=False)
-total_sold_by_product_df = total_sold_by_product_df.groupby('Tên sản phẩm')['Số lượng'].sum().sort_values(ascending=False).head(10).reset_index()
-print(total_sold_by_product_df)
-# Create a bar chart
-plt.figure(figsize=(12, 6))
-plt.bar(total_sold_by_product_df['Tên sản phẩm'], total_sold_by_product_df['Số lượng'])
+# -------------------------------------------------------------------------------------
+# total_sold_by_product_df = df.drop(columns=[
+#                                    'Thành tiền', 'Thời gian', 'Khách hàng', 'SĐT']).sort_values('Số lượng', ascending=False)
+# total_sold_by_product_df = total_sold_by_product_df.groupby(
+#     'Tên sản phẩm')['Số lượng'].sum().sort_values(ascending=False).head(10).reset_index()
+# print(total_sold_by_product_df)
+# # Create a bar chart
+# plt.figure(figsize=(12, 6))
+# plt.bar(total_sold_by_product_df['Tên sản phẩm'],
+#         total_sold_by_product_df['Số lượng'])
 
-# Add titles and labels
-plt.title('Top 10 Best Selling Products')
-plt.xlabel('Product Name')
-plt.ylabel('Total Quantity Sold')
-plt.xticks(rotation=45)
+# # Add titles and labels
+# plt.title('Top 10 sản phẩm bán chạy nhất')
+# plt.xlabel('Tên sản phảm')
+# plt.ylabel('Tổng số lượng bán được')
+# plt.xticks(rotation=45)
 
-# Show the plot
+# # Show the plot
+# plt.show()
+# plt.savefig('chart1.png', bbox_inches='tight')
+# total_sold_by_product_df.to_excel('chart1.xlsx', index=False)
+
+# -------------------------------------------------------------------------------------
+
+# product_revenue = df.groupby('Tên sản phẩm')['Thành tiền'].sum()
+
+# # Sorting the products by total revenue in descending order
+# product_revenue_sorted = product_revenue.sort_values(ascending=False)
+
+# # Selecting the product with the highest total sales value
+# top_product_revenue = product_revenue_sorted.head(1)
+
+# # Plotting the bar chart for the product with the highest total sales value
+# plt.figure(figsize=(6, 4))
+# plt.bar(top_product_revenue.index, top_product_revenue, color='blue')
+# plt.xlabel('Tên sản phẩm')
+# plt.ylabel('Tổng giá trị bán hàng (VND)')
+# plt.title('Sản phẩm có tổng giá trị bán hàng cao nhất')
+# plt.grid(axis='y')
+
+# # Formatting the y-axis to display full revenue values
+# plt.gca().get_yaxis().set_major_formatter(
+#     plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+
+# # Display the plot
+# plt.show()
+# product_revenue_df = product_revenue_sorted.reset_index()
+# product_revenue_df.columns = ['Tên sản phẩm', 'Tổng giá trị bán hàng']
+# product_revenue_df.to_excel('chart2.xlsx', index=False)
+# -------------------------------------------------------------------------------------
+
+# Grouping by customer's phone number and summing their total purchase value
+# customer_purchase = df.groupby('SĐT')['Thành tiền'].sum()
+
+# # Sorting the customers by total purchase value in descending order
+# customer_purchase_sorted = customer_purchase.sort_values(ascending=False)
+
+# # Creating a new DataFrame with phone numbers and their total purchase value
+# customer_purchase_df = customer_purchase_sorted.reset_index()
+# customer_purchase_df.columns = ['SĐT', 'Tổng giá trị mua hàng']
+
+# # Displaying the new DataFrame
+# customer_purchase_df.head()
+# customer_purchase_df.to_excel('chart2.xlsx', index=False)
+
+# -------------------------------------------------------------------------------------
+
+df['Thời gian'] = pd.to_datetime(df['Thời gian'])
+df['Giờ'] = df['Thời gian'].dt.hour
+
+# data_hours = df[(df['Giờ'] >= 6) & (df['Giờ'] <= 22)]
+
+# # Grouping by hour and summing the total sales value
+# hourly_sales = data_hours.groupby('Giờ')['Thành tiền'].sum()
+
+# # Sorting the hourly sales in descending order
+# hourly_sales_sorted = hourly_sales.sort_values(ascending=False)
+
+# # Plotting the bar chart for sales revenue by hour from 6 AM to 10 PM
+# plt.figure(figsize=(12, 6))
+# plt.bar(hourly_sales.index, hourly_sales, color='orange')
+# plt.xlabel('Giờ trong ngày')
+# plt.ylabel('Doanh thu (VND)')
+# plt.title('Doanh thu theo giờ từ 6h sáng đến 10h tối')
+# # Setting x-ticks for every hour from 6 AM to 10 PM
+# plt.xticks(np.arange(6, 23, 1))
+# plt.grid(axis='y')
+
+# # Formatting the y-axis to display full revenue values
+# plt.gca().get_yaxis().set_major_formatter(
+#     plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+
+# # Display the plot
+# plt.show()
+# hourly_sales_sorted_df = hourly_sales_sorted.reset_index()
+# # hourly_sales_sorted_df.columns = ['SĐT', 'Tổng giá trị mua hàng']
+# hourly_sales_sorted_df.to_excel('chart2.xlsx', index=False)
+
+# print(hourly_sales_sorted_df.head())
+
+# -------------------------------------------------------------------------------------
+
+# df['Thời gian'] = pd.to_datetime(df['Thời gian'])
+# df['Giờ'] = df['Thời gian'].dt.hour
+# data_morning = df[(df['Giờ'] >= 6) & (df['Giờ'] < 8)]
+
+# morning_product_sales = data_morning.groupby('Tên sản phẩm')['Số lượng'].sum()
+
+# morning_product_sales_sorted = morning_product_sales.sort_values(
+#     ascending=False)
+# morning_product_sales_sorted_df = morning_product_sales_sorted.reset_index()
+# morning_product_sales_sorted_df.to_excel('chart2.xlsx', index=False)
+
+# -------------------------------------------------------------------------------------
+
+# df['Thời gian'] = pd.to_datetime(df['Thời gian'])
+# df['Giờ'] = df['Thời gian'].dt.hour
+# data_evening = df[(df['Giờ'] >= 16) & (df['Giờ'] < 19)]
+
+# evening_product_sales = data_evening.groupby('Tên sản phẩm')['Số lượng'].sum()
+# evening_product_sales_sorted = evening_product_sales.sort_values(
+#     ascending=False)
+
+# evening_product_sales_sorted = evening_product_sales.reset_index()
+# evening_product_sales_sorted.to_excel('chart2.xlsx', index=False)
+# print(evening_product_sales_sorted.head())
+
+# -------------------------------------------------------------------------------------
+
+# product_name_to_code = {
+#     'Donut': 'DN001',
+#     'Cupcake': 'CCK001',
+#     'Macaron': 'MCR001',
+#     'Crepe': 'CR001',
+#     'Su Kem': 'SK001',
+#     'Red Velvet': 'RV001',
+#     'Panna Cotta': 'PNCT001',
+#     'Cookies': 'CK001',
+#     'Tiramisu': 'TRMS001'
+# }
+
+# # Mapping product names in the sales data to their product codes
+# df['Mã SP'] = df['Tên sản phẩm'].map(product_name_to_code)
+
+# # Extracting the product group code from the sales data
+# df['Nhóm sản phẩm'] = df['Mã SP'].str[:2]
+
+# # Grouping by product group and summing the total sales value
+# group_sales = df.groupby('Nhóm sản phẩm')['Thành tiền'].sum()
+
+# # Sorting the group sales in alphabetical order of the product group code
+# group_sales_sorted = group_sales.sort_index()
+
+# # Plotting the bar chart for sales revenue by product group
+# plt.figure(figsize=(10, 6))
+# plt.bar(group_sales_sorted.index, group_sales_sorted, color='cyan')
+# plt.xlabel('Nhóm sản phẩm')
+# plt.ylabel('Doanh thu (VND)')
+# plt.title('Doanh thu theo nhóm sản phẩm')
+# plt.grid(axis='y')
+
+# # Formatting the y-axis to display full revenue values
+# plt.gca().get_yaxis().set_major_formatter(
+#     plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+
+# # Display the plot
+# plt.show()
+# group_sales_sorted.reset_index().to_excel('chart2.xlsx', index=False)
+
+
+# -------------------------------------------------------------------------------------
+
+# product_name_to_code = {
+#     'Donut': 'DN001',
+#     'Cupcake': 'CCK001',
+#     'Macaron': 'MCR001',
+#     'Crepe': 'CR001',
+#     'Su Kem': 'SK001',
+#     'Red Velvet': 'RV001',
+#     'Panna Cotta': 'PNCT001',
+#     'Cookies': 'CK001',
+#     'Tiramisu': 'TRMS001'
+# }
+
+# df['Mã SP'] = df['Tên sản phẩm'].map(product_name_to_code)
+# df['Nhóm sản phẩm'] = df['Mã SP'].str[:2]
+# # Grouping data by product group and product name, then summing the total sales value
+# group_product_sales = df.groupby(['Nhóm sản phẩm', 'Tên sản phẩm'])[
+#     'Thành tiền'].sum()
+
+# # Getting unique product groups
+# unique_product_groups = group_product_sales.index.get_level_values(0).unique()
+
+# # Plotting pie charts for each product group
+# fig, axes = plt.subplots(len(unique_product_groups), 1,
+#                          figsize=(10, 6 * len(unique_product_groups)))
+# fig.tight_layout(pad=6.0)
+
+# for i, group in enumerate(unique_product_groups):
+#     # Selecting data for the specific product group
+#     group_data = group_product_sales[group].sort_values(ascending=False)
+
+#     # Plotting the pie chart
+#     axes[i].pie(group_data, labels=group_data.index,
+#                 autopct='%1.1f%%', startangle=140)
+#     axes[i].set_title(f'Doanh thu của các sản phẩm trong nhóm {group}')
+
+# # Show the plot
+# plt.show()
+# group_sales_sorted.reset_index().to_excel('chart2.xlsx', index=False)
+
+# -------------------------------------------------------------------------------------
+
+# Re-extracting the hour from the 'Thời gian' column
+df['Thời gian'] = pd.to_datetime(df['Thời gian'])
+df['Giờ'] = df['Thời gian'].dt.hour
+
+# Grouping by product name and hour, then summing the quantity sold
+product_hourly_sales = df.groupby(['Tên sản phẩm', 'Giờ'])[
+    'Số lượng'].sum().unstack(fill_value=0)
+
+# Plotting the bar chart for hourly sales of each product
+product_hourly_sales.plot(kind='bar', figsize=(15, 8), stacked=True)
+
+plt.xlabel('Tên sản phẩm')
+plt.ylabel('Số lượng bán ra')
+plt.title('Doanh số theo giờ của từng sản phẩm')
+plt.legend(title='Giờ trong ngày', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+
+# Display the plot
 plt.show()
+product_hourly_sales.reset_index().to_excel('chart2.xlsx', index=False)
--- main.py
+++ main.py
@@ -9,24 +9,32 @@ import string
 import os
 
 
-
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 df_Button = pd.read_excel("CSDL_MON.xlsx", index_col=None)
 
 
-
-df_Order = pd.DataFrame([], columns = ["STT","Tên sản phẩm",
-                                       "Đơn giá", "Số lượng",
-                                       "Thành tiền"])
+df_Order = pd.DataFrame([], columns=["STT", "Tên sản phẩm",
+                                     "Đơn giá", "Số lượng",
+                                     "Thành tiền"])
 
 root = Tk()
 root.geometry("800x600")
+root.title("Order System")
 
-leftFrame = Frame(root, bg="mistyrose", width=400, heigh=600)
+# Set a custom background color for the main window
+root.configure(bg="#F0F0F0")
+
+# Define custom fonts
+font_bold = ('Arial', 12, 'bold')
+font_normal = ('Arial', 12)
+
+# Create left and right frames with a light background color
+leftFrame = Frame(root, bg="#E0E0E0", width=400, height=600)
 leftFrame.grid(row=0, column=0)
-rightFrame = Frame(root, bg="mistyrose", width=400, heigh=600)
+rightFrame = Frame(root, bg="#E0E0E0", width=400, height=600)
 rightFrame.grid(row=0, column=1)
 tv_index = 0
+
 
 def BT_callback(m):
     global tv_index
@@ -40,7 +48,8 @@ def BT_callback(m):
         # Product exists, update quantity and total price
         df_Order.loc[df_Order["Tên sản phẩm"] == product_name, "Số lượng"] += 1
         df_Order.loc[df_Order["Tên sản phẩm"] == product_name, "Thành tiền"] = \
-            df_Order.loc[df_Order["Tên sản phẩm"] == product_name, "Số lượng"] * product_price
+            df_Order.loc[df_Order["Tên sản phẩm"] ==
+                         product_name, "Số lượng"] * product_price
     else:
         # New product, add to DataFrame
         print('add new')
@@ -67,6 +76,7 @@ def BT_callback(m):
     # Update the total price labels
     update_total_price_labels()
 
+
 def update_total_price_labels():
     total_price = df_Order["Thành tiền"].sum()
     LB_Sum_KQ.config(text=str(total_price) + "đ")
@@ -79,15 +89,19 @@ def NhapMoi_Callback():
     global df_Order
     tv.delete(*tv.get_children())
     tv_index = 0
-    df_Order = pd.DataFrame([], columns = ["STT","Tên sản phẩm",
-                                       "Đơn giá", "Số lượng",
-                                       "Thành tiền"])
+    df_Order = pd.DataFrame([], columns=["STT", "Tên sản phẩm",
+                                         "Đơn giá", "Số lượng",
+                                         "Thành tiền"])
+
+
 def ThanhToan_Callback():
     global df_Order
     global df_Banhang
-    submit_order= df_Order.copy()
-    submit_order.insert(loc = 0,column = 'Thời gian',value = datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
-    submit_order.insert(loc = 1,column = 'Mã đơn hàng',value = ''.join(random.choices(string.ascii_letters + string.digits, k=10)))
+    submit_order = df_Order.copy()
+    submit_order.insert(loc=0, column='Thời gian',
+                        value=datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+    submit_order.insert(loc=1, column='Mã đơn hàng', value=''.join(
+        random.choices(string.ascii_letters + string.digits, k=10)))
     submit_order['Khách hàng'] = str(ET_Customer.get())
     submit_order['SĐT'] = str(ET_Phone.get())
     submit_order.drop(columns=['STT'], inplace=True)
@@ -96,39 +110,40 @@ def ThanhToan_Callback():
     if os.path.isfile('a.xlsx'):
         df_Banhang = pd.read_excel('a.xlsx')
         df_Banhang = df_Banhang._append(submit_order)
-        df_Banhang.to_excel("a.xlsx",index=False)
+        df_Banhang.to_excel("a.xlsx", index=False)
     else:
-        submit_order.to_excel("a.xlsx",index=False)
-   
+        submit_order.to_excel("a.xlsx", index=False)
+
+
 # Button creation
 BT_list = []
 BT_Photo = []
 BT_Frame = []
 BT_Label = []
 BT_Price = []
-for i in range(0,df_Button.shape[0]):
-    BT_Frame.append(Frame(leftFrame,width=130, heigh = 200))
-    BT_Frame[i].grid(row = i//3, column = i%3)
+for i in range(0, df_Button.shape[0]):
+    BT_Frame.append(Frame(leftFrame, width=130, height=200))
+    BT_Frame[i].grid(row=i//3, column=i % 3)
     file_name = df_Button["Mã SP"][i].strip()+".png"
     # file_name = ''
     image = Image.open(file_name)
     photo = ImageTk.PhotoImage(image)
     BT_Photo.append(photo)
     BT_list.append(Button(BT_Frame[i],
-                          image = BT_Photo[i],
-                          command = lambda m=i: BT_callback(m)))
+                          image=BT_Photo[i],
+                          command=lambda m=i: BT_callback(m)))
     BT_list[i].grid(row=0, column=0)
-    BT_Label.append(Label(BT_Frame[i],text=df_Button["Tên sản phẩm"][i]))
-    BT_Label[i].grid(row=1,column=0)
-    BT_Price.append(Label(BT_Frame[i],text=df_Button["Đơn giá"][i]))
-    BT_Price[i].grid(row=2,column=0)
+    BT_Label.append(Label(BT_Frame[i], text=df_Button["Tên sản phẩm"][i]))
+    BT_Label[i].grid(row=1, column=0)
+    BT_Price.append(Label(BT_Frame[i], text=df_Button["Đơn giá"][i]))
+    BT_Price[i].grid(row=2, column=0)
 
-rightFrameUpper = Frame(rightFrame,width=400,heigh=400,bg='mistyrose')
+rightFrameUpper = Frame(rightFrame, width=400, height=400, bg='mistyrose')
 rightFrameUpper.grid(row=0, column=0)
 
 tv = ttk.Treeview(rightFrameUpper)
-tv['columns']=['STT', 'Tên sản phẩm', 'Đơn giá',
-               'Số lượng', 'Thành  tiền']
+tv['columns'] = ['STT', 'Tên sản phẩm', 'Đơn giá',
+                 'Số lượng', 'Thành  tiền']
 tv.column('#0', width=0, stretch=NO)
 tv.column('STT', anchor=CENTER, width=80)
 tv.column('Tên sản phẩm', anchor=CENTER, width=80)
@@ -144,36 +159,35 @@ tv.heading('Số lượng', text='Số lượng', anchor=CENTER)
 tv.heading('Thành  tiền', text='Thành  tiền', anchor=CENTER)
 tv.grid(row=0, column=0)
 
-rightFrameLower = Frame(rightFrame,width=400,heigh=200,bg='mistyrose')
+rightFrameLower = Frame(rightFrame, width=400, height=200, bg='mistyrose')
 rightFrameLower.grid(row=1, column=0)
-LB_Customer = Label(rightFrameLower,text = "Khách hàng:",bg ="pink",width=10)
+LB_Customer = Label(rightFrameLower, text="Khách hàng:", bg="pink", width=10)
 LB_Customer.grid(row=0, column=0)
-ET_Customer = Entry(rightFrameLower,width=20)
-ET_Customer.grid(row=1, column=0)
+ET_Customer = Entry(rightFrameLower, width=20, font=font_normal)
+ET_Customer.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
-LB_Phone = Label(rightFrameLower,text = "SĐT:",bg="pink")
+LB_Phone = Label(rightFrameLower, text="SĐT:", bg="pink")
 LB_Phone.grid(row=2, column=0)
-ET_Phone = Entry(rightFrameLower)
-ET_Phone.grid(row=3, column=0)
 
-LB_Sum = Label(rightFrameLower,text = "Cộng:",bg="lavenderblush",width=10)
-LB_Sum.grid(row=0, column=1)
-LB_VAT = Label(rightFrameLower,text = "VAT(10%)",bg="lavenderblush",width=10)
-LB_VAT.grid(row=1, column=1)
-LB_Total = Label(rightFrameLower,text = "Tổng cộng:",bg="lavenderblush",width=10)
-LB_Total.grid(row=2, column=1)
+ET_Phone = Entry(rightFrameLower, font=font_normal)
+ET_Phone.grid(row=3, column=0, padx=5, pady=5, sticky="w")
 
-LB_Sum_KQ = Label(rightFrameLower,text = "0đ",bg="lavenderblush",width=10)
-LB_Sum_KQ.grid(row=0, column=2)
-LB_VAT_KQ = Label(rightFrameLower,text = "0đ",bg="lavenderblush",width=10)
-LB_VAT_KQ.grid(row=1, column=2)
-LB_Total_KQ = Label(rightFrameLower,text = "0đ",bg="lavenderblush",width=10)
-LB_Total_KQ.grid(row=2, column=2)
+# Customize button appearance
+BT_Nhapmoi = Button(rightFrameLower, text="Nhập mới", bg="#FFD700",
+                    fg="black", font=font_bold, command=NhapMoi_Callback)
+BT_Nhapmoi.grid(row=4, column=0, pady=10)
+BT_ThanhToan = Button(rightFrameLower, text="Thanh Toán", bg="#32CD32",
+                      fg="white", font=font_bold, command=ThanhToan_Callback)
+BT_ThanhToan.grid(row=4, column=1, columnspan=2, pady=10)
 
-BT_Nhapmoi = Button(rightFrameLower,text = "Nhập mới", bg="lavender",
-                    command = NhapMoi_Callback)
-BT_Nhapmoi.grid(row=4, column=0)
-BT_ThanhToan = Button(rightFrameLower,text = "Thanh Toán", bg="oldlace",
-                      command = ThanhToan_Callback)
-BT_ThanhToan.grid(row=4, column=1, columnspan=2)
+# Modify the appearance of labels
+LB_Sum_KQ = Label(rightFrameLower, text="0đ", bg="#E0E0E0", font=font_normal)
+LB_Sum_KQ.grid(row=0, column=2, pady=5)
+LB_VAT_KQ = Label(rightFrameLower, text="0đ", bg="#E0E0E0", font=font_normal)
+LB_VAT_KQ.grid(row=1, column=2, pady=5)
+LB_Total_KQ = Label(rightFrameLower, text="0đ", bg="#E0E0E0", font=font_normal)
+LB_Total_KQ.grid(row=2, column=2, pady=5)
+
+# Modify entry field appearance
+
 root.mainloop()
